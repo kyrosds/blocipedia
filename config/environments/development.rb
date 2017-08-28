105e57865b0fa769aab639f90a0620311e29e249
David Sires <d.sires@yahoo.com>
2017-08-27 17:32:14 -0700

    User-Sign-Up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -37,5 +39,5 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  # config.action_view.raise_on_missing_translations = truer
 end
