a407a3856546cd35d5a761b89728679c9b8babc1
David Sires <d.sires@yahoo.com>
2017-10-01 15:11:44 -0700

    Policy-Update

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -14,7 +14,7 @@ class WikiPolicy < ApplicationPolicy
       elsif user && user.role == 'premium'
         all_wikis = scope.all
         all_wikis.each do |wiki|
-          if !wiki.private? || wiki.user == user || wiki.collaborators.include?(user)
+          if wiki.private? || wiki.user == user || wiki.collaborators.include?(user)
             wikis << wiki
           end
         end
@@ -22,7 +22,7 @@ class WikiPolicy < ApplicationPolicy
         all_wikis = scope.all
         wikis = []
         all_wikis.each do |wiki|
-          if !wiki.private? || wiki.collaborators.include?(user)
+          if wiki.private? || wiki.collaborators.include?(user)
             wikis << wiki
           end
         end
